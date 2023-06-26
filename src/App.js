9f05d92c2f65ba3d5dd40565b284ba6cb3d5b7ce
parbat <parbat.chhetri79@gmail.com>
2023-06-26 15:14:54 +0545

    add

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Contact from '../src/components/Home/Contact';
 function App() {
   return (
     <div className='bg-[#1E1E1F] text-white'>
-     ad<Contact/>
+     <Contact/>
       </div>
 );
 }
