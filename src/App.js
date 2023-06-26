5804601050f2fd991bcf33d166aac4e2856f7dd8
parbat <parbat.chhetri79@gmail.com>
2023-06-26 15:12:08 +0545

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-
+import Contact from '../src/components/Home/Contact';
 function App() {
   return (
-    <div className="App">
-      parvat nkjsnva
-    </div>
-  );
+    <div className='bg-[#1E1E1F] text-white'>
+<Contact/>
+      </div>
+);
 }
 
 export default App;
