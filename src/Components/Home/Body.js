7d0d2b3fc53dcfdda59a48cd942a259db0347bfd
Eliza Pant <elizapant.github@gmail.com>
2023-06-26 16:02:11 +0545

    main page(still work to be done)

---
+++ src/Components/Home/Body.js
@@ -0,0 +1,80 @@
+import React from 'react'
+
+export default function Body() {
+  return (
+   
+      <div className='flex bg-gray-900 '>
+        <div className='bg-gray-900 w-[350px]'>Sidebar</div>
+        <div className='grid grid-cols-2 justify-center gap-12 pt-[40px] pl-[25px]  '>
+        <div className='w-[420px] h-[280px] rounded-xl relative'>
+  <img src="https://images.unsplash.com/photo-1550828486-68812fa3f966?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" className='w-[420px] h-[280px] rounded-xl border border-gray-900 relative' />
+  <div className='bg-gradient-to-r from-cyan-500 to-blue-500 absolute w-[420px] h-[280px] rounded-xl border border-gray-900 z-10 opacity-0 bg-transparent'></div>
+  <div className=' '>
+    <div className='border border-green-500 rounded-3xl bg-green-500 text-black font-semibold top-5 absolute right-2 px-3 py-2'>Online Gaming</div>
+    <div className='text-white font-semibold bottom-28 absolute left-5'>May 20, 2023</div>
+    <div className='text-white font-semibold bottom-10 absolute left-5 text-2xl pr-8'>Influence of Online Game Behaviors on Emotional States</div>
+  </div>
+</div>
+
+        <div className='w-[420px] h-[280px] rounded-xl relative'>
+        <img src="https://media.istockphoto.com/id/1145166058/photo/retro-old-microphone-vintage-style-with-neon-lights.webp?b=1&s=170667a&w=0&k=20&c=9dfmza0BqYIHatlP5lhoMP1v8X6kvFdqLBDeactXikM=" className='w-[420px] h-[280px]  rounded-xl border border-gray-900'/>
+        <div className=' '>
+    <div className='border border-green-500 rounded-3xl bg-green-500 text-black font-semibold  top-5 absolute right-2 px-3 py-2'>Entertainment</div>
+    <div className='text-white font-semibold bottom-28 absolute left-5 '> May 20, 2023</div>
+    <div className='text-white font-semibold bottom-10 absolute left-5 text-2xl pr-8'> Music changes the world, music for everybody </div>
+  </div>
+        </div>
+
+
+        <div className='w-[420px] h-[280px] rounded-xl  relative '>
+        <img src="https://media.istockphoto.com/id/1486013436/photo/summer-holiday-travel-objects-on-neon-lighting-background.webp?b=1&s=170667a&w=0&k=20&c=4ORbBRMsZNB4wcdDg6w5qo2DM9Xyt4xEDjp8Aue6kg8=" className='w-[420px] h-[280px]  rounded-xl border border-gray-900'/>
+        <div className=' '>
+    <div className='border border-green-500 rounded-3xl bg-green-500 text-black font-semibold  top-5 absolute right-2 px-3 py-2'>Explore</div>
+    <div className='text-white font-semibold bottom-28 absolute left-5'> May 20, 2023</div>
+    <div className='text-white font-semibold bottom-10 absolute left-5 text-2xl pr-8'> Explore the world with the best guidance ever </div>
+  </div>
+        
+        </div>
+        <div className='w-[420px] h-[280px] rounded-xl relative '>
+        <img src="https://media.istockphoto.com/id/1388988332/photo/cyborg-running-on-purple-background-3d-wired-shape.webp?b=1&s=170667a&w=0&k=20&c=ZsKLspVyfbxfSGHTEHMiqQWRXGAKxuhqM6fLJnnb5ok=" className='w-[420px] h-[280px]  rounded-xl border border-gray-900'/>
+        <div className=' '>
+    <div className='border border-green-500 rounded-3xl bg-green-500 text-black font-semibold  top-5 absolute right-2 px-3 py-2'>Artificial Intelligence</div>
+    <div className='text-white font-semibold bottom-28 absolute left-5'> May 20, 2023</div>
+    <div className='text-white font-semibold bottom-10 absolute left-5 text-2xl pr-8'> AI powered Personal Assistant for Windows </div>
+  </div>
+        </div>
+
+        <div className='w-[420px] h-[280px] rounded-xl relative'>
+        <img src="https://media.istockphoto.com/id/1284053136/photo/young-sportive-woman-training-isolated-on-gradient-studio-background-in-neon-light-athletic.webp?b=1&s=170667a&w=0&k=20&c=JUJKEBWcf-PhGHL5Ap-aSVwafLUnxOrlH6Zod_pHSMo=" className='w-[420px] h-[280px]  rounded-xl border border-gray-900'/>
+
+        <div className=' '>
+    <div className='border border-green-500 rounded-3xl bg-green-500 text-black font-semibold  top-5 absolute right-2 px-3 py-2'>Fitness</div>
+    <div className='text-white font-semibold bottom-28 absolute left-5'> May 20, 2023</div>
+    <div className='text-white font-semibold bottom-10 absolute left-5 text-2xl pr-8'> Prepare your fitness now, Life Hacks to rule </div>
+  </div>
+        </div>
+
+
+
+        <div className='w-[420px] h-[280px] rounded-xl relative'>
+  <img
+    src="https://images.unsplash.com/photo-1475483768296-6163e08872a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+    className='w-[420px] h-[280px] rounded-xl border border-gray-900'
+    alt="Image"
+  />
+  <div className=' '>
+    <div className='border border-green-500 rounded-3xl bg-green-500 text-black font-semibold  top-5 absolute right-2 px-3 py-2'>Travelling</div>
+    <div className='text-white font-semibold bottom-28 absolute left-5'> April 29, 2023</div>
+    <div className='text-white font-semibold bottom-10 absolute left-5 text-2xl pr-8'> National Parks in California, A Guide for Nature Lovers</div>
+  </div>
+</div>
+
+        
+        
+      
+       </div>
+
+      </div>
+   
+  )
+}
