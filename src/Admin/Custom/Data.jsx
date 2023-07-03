adea47420753a7ed06a9317ecf5b3945219d0e2d
Parbat Chhteri <parbat.chhetri79@gmail.com>
2023-07-03 11:33:56 +0545

    Update Data.jsx

--- src/Admin/Custom/Data.jsx
+++ src/Admin/Custom/Data.jsx
@@ -1,7 +1,106 @@
-import React from 'react'
+import photo1 from "../../assets/Contact/photo1.png"
+import photo2 from "../../assets/Contact/photo2.png"
+import photo3 from "../../assets/Contact/photo3.png"
+import photo4 from "../../assets/Contact/photo4.png"
+import photo5 from "../../assets/Contact/photo5.png"
+import photo6 from "../../assets/Contact/photo6.png"
+import photo7 from "../../assets/Contact/photo7.png"
+import photo8 from "../../assets/Contact/photo8.png"
+import photo9 from "../../assets/Contact/photo9.png"
+import photo10 from "../../assets/Contact/photo10.png"
+import photo11 from "../../assets/Contact/photo11.png"
+import photo12 from "../../assets/Contact/photo12.png"
 
-export default function Data() {
-  return (
-    <div>Data</div>
-  )
-}
+import {TiTick} from "react-icons/ti"
+import {BiSolidInfoCircle} from "react-icons/bi"
+const Data = [
+   {
+       image: photo1,
+       icon: <TiTick/>,
+       desc: "Active",
+       icon1: <BiSolidInfoCircle/>,
+       names: "air",
+   },
+   {
+      image: photo2,
+      icon: <TiTick/>,
+      desc: "Active",
+      icon1: <BiSolidInfoCircle/>,
+      names: "air",
+  },
+  {
+   image: photo3,
+   icon: <TiTick/>,
+   desc: "Active",
+   icon1: <BiSolidInfoCircle/>,
+   names: "air",
+},
+{
+   image: photo4,
+   icon: <TiTick/>,
+   desc: "Active",
+   icon1: <BiSolidInfoCircle/>,
+   names: "air",
+},
+{
+   image: photo5,
+   icon: <TiTick/>,
+   desc: "Active",
+   icon1: <BiSolidInfoCircle/>,
+   names: "air",
+},
+{
+   image: photo6,
+   icon: <TiTick/>,
+   desc: "Active",
+   icon1: <BiSolidInfoCircle/>,
+   names: "air",
+},
+{
+   image: photo7,
+   icon: <TiTick/>,
+   desc: "Active",
+   icon1: <BiSolidInfoCircle/>,
+   names: "air",
+},
+{
+   image: photo8,
+   icon: <TiTick/>,
+   desc: "Active",
+   icon1: <BiSolidInfoCircle/>,
+   names: "air",
+},
+{
+   image: photo9,
+   icon: <TiTick/>,
+   desc: "Active",
+   icon1: <BiSolidInfoCircle/>,
+   names: "air",
+},
+{
+   image: photo10,
+   icon: <TiTick/>,
+   desc: "Active",
+   icon1: <BiSolidInfoCircle/>,
+   names: "air",
+},
+{
+   image: photo11,
+   icon: <TiTick/>,
+   desc: "Active",
+   icon1: <BiSolidInfoCircle/>,
+   names: "air",
+},
+{
+   image: photo12,
+   icon: <TiTick/>,
+   desc: "Active",
+   icon1: <BiSolidInfoCircle/>,
+   names: "air",
+},
+
+
+];
+
+
+export default Data
