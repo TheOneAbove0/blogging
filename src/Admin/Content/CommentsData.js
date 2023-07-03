bffd3756b9bf17f77e4d5f57c653cd7992e9bea9
Eliza Pant <108278876+panteliza@users.noreply.github.com>
2023-07-03 15:51:29 +0545

    Update CommentsData.js
    addwd

--- src/Admin/Content/CommentsData.js
+++ src/Admin/Content/CommentsData.js
@@ -1,7 +1,63 @@
-import React from 'react'
+const comments = [
+  { id: 1,
+  title: 'Einride brings its electric self driving trucks to the UAE', 
+ author: 56 ,
 
-export default function CommentsData() {
-  return (
-    <div>CommentsData</div>
-  )
-}
+  date:"2023-05-30",
+  },
+  { id: 2,
+    title: 'AI powered Personal Assistant for Windows', 
+   author: 5 ,
+ 
+    date:"2023-05-30",
+    },
+    { id: 3,
+      title: 'Start a Podcast and Build a Loyal Audience in No Time', 
+     author: 6 ,
+   
+      date:"2023-05-30",
+      },
+      { id: 4,
+        title: 'National Parks in California, A Guide for Nature Lovers', 
+       author: 8 ,
+       
+        date:"2023-05-30",
+        },
+        { id: 5,
+          title: ' Secrets of Successful Entrepreneurs You Need to Know', 
+         author: 9 ,
+       
+          date:"2023-05-30",
+          },
+          { id: 6,
+            title: ' Start a Podcast and Build a Loyal Audience in No Time', 
+           author: 4 ,
+       
+            date:"2023-05-30",
+            },
+            { id: 7,
+              title: ' C# 11, The Latest and Greatest Features with Code Examples', 
+             author: 7 ,
+            
+              date:"2023-05-30",
+              },
+              { id: 8,
+                title: ' Secrets of Successful Entrepreneurs You Need to Know', 
+               author: 5 ,
+              
+                date:"2023-05-30",
+                },
+              { id: 9,
+                title: 'National Parks in California, A Guide for Nature Lovers', 
+                 author: 2 ,
+             
+                date:"2023-05-30",
+                  },
+                { id: 10,
+                  title: 'AI powered Personal Assistant for Windows', 
+                  author: 12 ,
+                  
+                  date:"2023-05-30",
+                 },
+];
+ export default comments;
